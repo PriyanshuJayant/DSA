2263b2780bfd1797a233e76cf629d70be0d45ef8
Priyanshu Jayant <priyanshujayant8@gmail.com>
2025-09-10 23:40:38 +0530

    Leet code problem 7 Solved

---
+++ LeetCode/7.cpp
@@ -0,0 +1,56 @@
+// 7. Reverse Integer
+#include<iostream>
+#include<climits>
+using namespace std;
+
+// Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+// Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+// Example 1:
+
+// Input: x = 123
+// Output: 321
+// Example 2:
+
+// Input: x = -123
+// Output: -321
+// Example 3:
+
+// Input: x = 120
+// Output: 21
+
+class Solution {
+public:
+    int reverse(int x) {
+        long long int temp = 0,count = 0;
+        long long int len = abs((long long) x);
+        while(len > 0){
+            len /= 10;
+            count++;
+        }
+        for (long long int i = 0; i < count; i++){
+            temp = temp * 10;
+            temp += x % 10;
+            x /= 10;
+        }
+        if (temp >= INT_MAX || temp <= INT_MIN) return 0;
+        x = temp;
+        return x;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    // Test cases
+    int testCases[] = {120, -123, 123, 1534236469, -2147483648};
+    int n = sizeof(testCases) / sizeof(testCases[0]);
+
+    for (int i = 0; i < n; i++) {
+        int input = testCases[i];
+        int output = sol.reverse(input);
+        cout << "Input: " << input << " → Output: " << output << endl;
+    }
+
+    return 0;
+}
--- Practice/reverseNumber.cpp
+++ Practice/reverseNumber.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 using namespace std;
 
-void reverseNumber(int n)
-{
+void reverseNumber(int n){
     int temp = 0;
-
     for (int i = 0; i < 3; i++)
     {
         temp = temp * 10;
@@ -14,8 +12,20 @@ void reverseNumber(int n)
     cout << temp;
 }
 
+void reverseNumber2(int n){
+    long long int len = abs(n);
+    long long int count = 0;
+    while (len > 0){
+        len /= 10;
+        count ++;
+    }
+    cout<<count;
+    
+}
+
 int main()
 {
-    int n = 123;
-    reverseNumber(n);
+    int n = 1534236469;
+    // reverseNumber(n);
+    reverseNumber2(n);
 }
